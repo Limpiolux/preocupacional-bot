2d4d0d9ef58b781c8299cb8027658c77d26b0bd0
Limpiolux <150255846+Limpiolux@users.noreply.github.com>
2023-11-30 13:41:50 -0300

    v1.0
    Versión estable del bot.

---
+++ src/infrastructure/repositories/ws.external.ts
@@ -0,0 +1,77 @@
+import { Client, LocalAuth } from "whatsapp-web.js";
+import { image as imageQr } from "qr-image";
+import LeadExternal from "../../domain/lead-external.repository";
+import qrCodeTerminal from "qrcode-terminal";
+
+/**
+ * Extendemos los super poderes de whatsapp-web
+ */
+class WsTransporter extends Client implements LeadExternal {
+  private status = false;
+
+  constructor() {
+    super({
+      authStrategy: new LocalAuth(),
+      puppeteer: {
+        headless: true,
+        args: [
+          "--disable-setuid-sandbox",
+          "--unhandled-rejections=strict",
+        ],
+      },
+    });
+
+    console.log("Iniciando....");
+
+    this.initialize();
+
+    this.on("ready", () => {
+      this.status = true;
+      console.log("LOGIN_SUCCESS");
+    });
+
+    this.on("auth_failure", () => {
+      this.status = false;
+      console.log("LOGIN_FAIL");
+    });
+
+    this.on("qr", (qr) => {
+      console.log("Escanea el código QR que está en la carpeta tmp");
+      this.generateImageAndPrintToConsole(qr);
+    });
+  }
+
+  /**
+   * Enviar mensaje de WS
+   * @param lead
+   * @returns
+   */
+  async sendMsg(lead: { message: string; phone: string }): Promise<any> {
+    try {
+      if (!this.status) return Promise.resolve({ error: "WAIT_LOGIN" });
+      const { message, phone } = lead;
+      const response = await this.sendMessage(`${phone}@c.us`, message);
+      return { id: response.id.id };
+    } catch (e: any) {
+      return Promise.resolve({ error: e.message });
+    }
+  }
+
+  getStatus(): boolean {
+    return this.status;
+  }
+
+  private generateImageAndPrintToConsole = (base64: string) => {
+    const path = `${process.cwd()}/tmp`;
+    let qr_svg = imageQr(base64, { type: "svg", margin: 4 });
+    qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
+
+    // Imprimir el QR en la consola usando qrcode-terminal
+    qrCodeTerminal.generate(base64, { small: true });
+
+    console.log(`⚡ Recuerda que el QR se actualiza cada minuto ⚡`);
+    console.log(`⚡ Actualiza F5 el navegador para mantener el mejor QR ⚡`);
+  };
+}
+
+export default WsTransporter;
